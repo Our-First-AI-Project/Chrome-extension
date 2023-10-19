00f9500b68a31b5e5b116d1186a7d11c0de276cd
yoouyeon <jyeon.yoon59@gmail.com>
2023-10-19 22:38:57 +0900

    Feat : div 태그 크롤링 후 제거

--- script/contentscript.js
+++ script/contentscript.js
@@ -1,7 +1,17 @@
 const removeAds = () => {
+  // img 태그 제거
   let imgs = document.querySelectorAll("img");
   imgs.forEach((img) => {
     img.remove();
+  });
+  // div 태그 제거
+  let divs = document.querySelectorAll("div");
+  divs.forEach((div) => {
+    const divSrc = div.style.backgroundImage.split('"')[1];
+    if (divSrc) {
+      // 스포츠 조선 광고 삽입 특성을 고려하여 이미지의 부모 element 제거
+      div.parentElement.remove();
+    }
   });
 };
 
