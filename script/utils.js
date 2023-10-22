1f70e379f9b7127573181fadb172b56e4bace8e8
yoouyeon <jyeon.yoon59@gmail.com>
2023-10-23 08:16:12 +0900

    Feat : checkCurrentTabUrl popup 페이지에 적용

--- script/utils.js
+++ script/utils.js
@@ -1,4 +1,4 @@
-export async function getCurrentTab() {
+async function getCurrentTab() {
   let queryOptions = { active: true, lastFocusedWindow: true };
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab;
