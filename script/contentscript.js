645a7b04582802da9d2a8b758cd9b16e13604ae9
yoouyeon <jyeon.yoon59@gmail.com>
2023-10-28 11:38:12 +0900

    Feat : 분류 결과 표시 기능 추가

--- script/contentscript.js
+++ script/contentscript.js
@@ -13,18 +13,42 @@ const removeComponent = async (component, url, option) => {
   return;
 };
 
-const removeAds = () => {
+const markComponent = async (component, url, option) => {
+  const isAd = await chrome.runtime.sendMessage({
+    option: "isAd",
+    url: url,
+  });
+  if (isAd === true) {
+    component.style.border = "5px solid #F266AB";
+    component.style.boxSizing = "border-box";
+  } else {
+    component.style.border = "5px solid #2CD3E1";
+    component.style.boxSizing = "border-box";
+  }
+  return;
+};
+
+const mainWorker = (workType, img, src, tagType) => {
+  if (workType === "mark") {
+    markComponent(img, src);
+  } else {
+    // workType === "remove" or undefined
+    removeComponent(img, src, tagType);
+  }
+};
+
+const workStart = (workType) => {
   // img 태그 제거
   let imgs = document.querySelectorAll("img");
   imgs.forEach((img) => {
-    removeComponent(img, img.src, "img");
+    mainWorker(workType, img, img.src, "img");
   });
   // div 태그 제거
   let divs = document.querySelectorAll("div");
   divs.forEach((div) => {
     const divSrc = div.style.backgroundImage.split('"')[1];
     if (divSrc) {
-      removeComponent(div, divSrc, "div");
+      mainWorker(workType, div, divSrc, "div");
     }
   });
 };
@@ -34,9 +58,10 @@ window.onload = async function () {
     option: "isSupportedURL",
   });
   const isOnData = await chrome.storage.local.get("isOn");
+  const optionData = await chrome.storage.local.get("option");
   const isEmptyData = Object.keys(isOnData).length === 0;
   // TODO :popup에서 토글을 클릭하지 않으면 isOn이 {}로 저장되는 문제 해결
   if (isSupportedURL === true && (isEmptyData || isOnData.isOn === true)) {
-    removeAds();
+    workStart(optionData.option);
   }
 };
